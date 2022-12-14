174c4ee38de9c12483290196dbd68f3d21a88dd8
pri65 <priyankasoniii65@gmail.com>
2022-12-14 14:05:02 +0530

    new

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 const endDate = "1 Jan 2023 12:00 AM"; 
 
 document.getElementById("end-date").innerText=endDate;
+
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    //console.log(end,now)
+
+    //our difference is in milliseconds so we converted it into seconds by dividing it by 1000 
+    const diff = (end-now)/1000;
+
+    //convert into days 
+
+}
+
+clock()
+
+
+// 1 day = 24hrs
+// i hour = 60mins
+// 60 mins = 3600sec
